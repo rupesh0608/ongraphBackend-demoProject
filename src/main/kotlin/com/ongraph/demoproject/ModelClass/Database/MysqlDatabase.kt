7273d7999172ec569cc802307fc0471feb84845b
Rupesh Kumar deshmukh <rupeshdeshmukh618@gmail.com>
2022-01-12 10:43:11 +0530

    mysql library added

--- src/main/kotlin/com/ongraph/demoproject/ModelClass/Database/MysqlDatabase.kt
+++ src/main/kotlin/com/ongraph/demoproject/ModelClass/Database/MysqlDatabase.kt
@@ -7,21 +7,19 @@ import java.util.*
 class MysqlDatabase {
 
     fun getConnection(): Connection? {
-         var conn: Connection?=null
+       lateinit var conn: Connection
 
         val connectionProps = Properties()
         connectionProps["user"] = "admin"
         connectionProps["password"] = "qwertyuiop"
 
         val dbname = "demokotlinproject"
-    try {
-        Class.forName("com.mysql.jdbc.Driver").newInstance()
+
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance()
         conn = DriverManager.getConnection(
                 "jdbc:mysql://ongraph.coq4newt9li2.ap-south-1.rds.amazonaws.com:3306/$dbname",
                 "admin", "qwertyuiop")
-    }catch (e:Exception){
-        println(e.message)
-    }
+
         return conn
     }
 
